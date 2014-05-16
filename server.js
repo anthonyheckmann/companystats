7b70bce8361bb30f8a7abc34ade4179bf44699aa
Anthony Heckmann <anthony.heckmann@gmail.com>
2014-05-15 22:08:00 -0400

    got jsdom working to load website data

--- server.js
+++ server.js
@@ -30,40 +30,45 @@ var sockets = [];
 
 
 // ##Scraper
-function printTop3(agent) { 
-  var window = jsdom(agent.body).createWindow()
-    , $ = require('jquery').create(window);
-  
-  //
-  // Now you can use jQuery to your heart's content!
-  //
-  var titles = $('.title a')
-    , points = $('.subtext span');
-    
-  var printme = $.map(points, function(el, i) {
-      if (i < 3) {
-        return $(el).text() + '\t' + $(titles[i]).text();
-      }
+function printTop3(agent) {
+    jsdom.env(
+    agent.body, ["http://code.jquery.com/jquery.js"],
+
+    function(errors, window) {
+
+        $ = window.$;
+        //
+        // Now you can use jQuery to your heart's content!
+        //
+        var titles = $('.title a'),
+            points = $('.subtext span');
+
+        var printme = $.map(points, function(el, i) {
+            if (i < 3) {
+                return $(el).text() + '\t' + $(titles[i]).text();
+            }
+        });
+
+        console.log(printme.join('\n'));
     });
-  
-  console.log(printme.join('\n'));
 }
- 
+
 var urls = ['', 'newest'];
 var agent = httpAgent.create('news.ycombinator.com', urls);
 console.log('Scraping', urls.length, 'pages from', agent.host);
- 
-agent.addListener('next', function (err, agent) {
-  printTop3(agent);
-  console.log();
-  agent.next();
+
+agent.addListener('next', function(err, agent) {
+    printTop3(agent);
+    console.log();
+    agent.next();
 });
- 
-agent.addListener('stop', function (err, agent) {
-  if (err) console.log(err);
-  console.log('All done!');
+
+agent.addListener('stop', function(err, agent) {
+    if (err)
+        console.log(err);
+    console.log('All done!');
 });
- 
+
 // Start scraping
 agent.start();
 
