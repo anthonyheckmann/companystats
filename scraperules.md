86e063c53ac8de147ab9b930ca91f2d1f21cfa8a
Anthony Heckmann <anthony.heckmann@gmail.com>
2014-05-15 20:58:08 -0400

    added page fetching code and dependencies

---
+++ scraperules.md
@@ -0,0 +1,7 @@
+
+
+Twitter Followers
+$("td:nth-child(1) .js-mini-profile-stat").text();
+
+LinkedIn Employess
+$("li+ li .density").text();
--- server.js
+++ server.js
@@ -3,13 +3,16 @@
 //
 // A simple chat server using Socket.IO, Express, and Async.
 //
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-var sqlite = require('sqlite3'); 
+var http = require('http'),
+    path = require('path'),
+    async = require('async'),
+    socketio = require('socket.io'),
+    express = require('express'),
+    sqlite = require('sqlite3'),
+    util = require('util'),
+    url = require('url'),
+    httpAgent = require('http-agent'),
+    jsdom = require('jsdom').jsdom;
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -24,6 +27,58 @@ var io = socketio.listen(server);
 router.use(express.static(path.resolve(__dirname, 'client')));
 var messages = [];
 var sockets = [];
+
+
+// ##Scraper
+function printTop3(agent) { 
+  var window = jsdom(agent.body).createWindow()
+    , $ = require('jquery').create(window);
+  
+  //
+  // Now you can use jQuery to your heart's content!
+  //
+  var titles = $('.title a')
+    , points = $('.subtext span');
+    
+  var printme = $.map(points, function(el, i) {
+      if (i < 3) {
+        return $(el).text() + '\t' + $(titles[i]).text();
+      }
+    });
+  
+  console.log(printme.join('\n'));
+}
+ 
+var urls = ['', 'newest'];
+var agent = httpAgent.create('news.ycombinator.com', urls);
+console.log('Scraping', urls.length, 'pages from', agent.host);
+ 
+agent.addListener('next', function (err, agent) {
+  printTop3(agent);
+  console.log();
+  agent.next();
+});
+ 
+agent.addListener('stop', function (err, agent) {
+  if (err) console.log(err);
+  console.log('All done!');
+});
+ 
+// Start scraping
+agent.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 io.on('connection', function (socket) {
     messages.forEach(function (data) {
